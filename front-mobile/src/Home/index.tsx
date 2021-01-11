f2a352204986cc987ead867d4f5dbc16108b8291
RicardoBrunoB <ricardobrunob@gmail.com>
2021-01-11 17:52:50 -0300

    Add orders list

--- front-mobile/src/Home/index.tsx
+++ front-mobile/src/Home/index.tsx
@@ -1,16 +1,20 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
-import { Header } from 'react-native/Libraries/NewAppScreen';
+import Header from '../Header';
 
 function Home() {
 
-    const handleOnPress = () => {
+    const navigation = useNavigation();
 
+    const handleOnPress = () => {
+        navigation.navigate('Orders');
     }
 
   return (
     <>
+    <Header />
     <View style={styles.container}>
         {<Image source={require('../assets/deliveryman.png')} />}
         <Text style={styles.title}>Acompanhe os pedidos e {'\n'} entregue no prazo!</Text>
